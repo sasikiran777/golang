cc0a23b374d3a11dcc6c4380cef05149fd2749a2
Sasi <sasi@macbook>
2022-05-22 19:35:00 +0530

    added

--- 13. Interfaces.go
+++ 13. Interfaces.go
@@ -8,10 +8,20 @@ func main() {
 	fmt.Println("Interfaces intro")
 	var w Writer = ConsoleWriter{}
 	w.Write([]byte("Hello World"))
+	// Integer interface
+	myInt := IntCounter(0)
+	var incrementer Incrementer = &myInt
+	for i := 0; i < 5; i++ {
+		fmt.Println(incrementer.Increment())
+	}
 }
 
 type Writer interface {
 	Write([]byte) (int, error)
+}
+
+type Incrementer interface {
+	Increment() int
 }
 
 type ConsoleWriter struct{}
@@ -19,4 +29,11 @@ type ConsoleWriter struct{}
 func (c ConsoleWriter) Write(data []byte) (int, error) {
 	n, err := fmt.Println(data)
 	return n, err
+}
+
+type IntCounter int
+
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
 }
